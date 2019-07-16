397cd7b309db8b0f1e22c6bf550a9261f8b7f664
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-16 15:38:25 +0000

    IexQuote DTO added

---
+++ src/main/java/ca/jrvs/apps/trading/model/domain/IexQuote.java
@@ -0,0 +1,375 @@
+package ca.jrvs.apps.trading.model.domain;
+
+public class IexQuote implements Entity<String> {
+
+    private String avgTotalVolume;
+    private String calculationPrice;
+    private String change;
+    private String changePercent;
+    private String close;
+    private String closeTime;
+    private String companyName;
+    private String delayedPrice;
+    private String delayedPriceTime;
+    private String extendedChange;
+    private String extendedChangePercent;
+    private String extendedPrice;
+    private String extendedPriceTime;
+    private String high;
+    private String iexAskPrice;
+    private String iexAskSize;
+    private String iexBidPrice;
+    private String iexBidSize;
+    private String iexLastUpdated;
+    private String iexMarketPercent;
+    private String iexRealtimePrice;
+    private String iexRealtimeSize;
+    private String iexVolume;
+    private String latestPrice;
+    private String latestSource;
+    private String latestTime;
+    private String latestUpdate;
+    private String latestVolume;
+    private String low;
+    private String marketCap;
+    private String open;
+    private String openTime;
+    private String peRatio;
+    private String previousClose;
+    private String primaryExchange;
+    private String sector;
+    private String symbol;
+    private String week52High;
+    private String week52Low;
+    private String ytdChange;
+
+    public String getAvgTotalVolume() {
+        return avgTotalVolume;
+    }
+
+    public void setAvgTotalVolume(String avgTotalVolume) {
+        this.avgTotalVolume = avgTotalVolume;
+    }
+
+    public String getCalculationPrice() {
+        return calculationPrice;
+    }
+
+    public void setCalculationPrice(String calculationPrice) {
+        this.calculationPrice = calculationPrice;
+    }
+
+    public String getChange() {
+        return change;
+    }
+
+    public void setChange(String change) {
+        this.change = change;
+    }
+
+    public String getChangePercent() {
+        return changePercent;
+    }
+
+    public void setChangePercent(String changePercent) {
+        this.changePercent = changePercent;
+    }
+
+    public String getClose() {
+        return close;
+    }
+
+    public void setClose(String close) {
+        this.close = close;
+    }
+
+    public String getCloseTime() {
+        return closeTime;
+    }
+
+    public void setCloseTime(String closeTime) {
+        this.closeTime = closeTime;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getDelayedPrice() {
+        return delayedPrice;
+    }
+
+    public void setDelayedPrice(String delayedPrice) {
+        this.delayedPrice = delayedPrice;
+    }
+
+    public String getDelayedPriceTime() {
+        return delayedPriceTime;
+    }
+
+    public void setDelayedPriceTime(String delayedPriceTime) {
+        this.delayedPriceTime = delayedPriceTime;
+    }
+
+    public String getExtendedChange() {
+        return extendedChange;
+    }
+
+    public void setExtendedChange(String extendedChange) {
+        this.extendedChange = extendedChange;
+    }
+
+    public String getExtendedChangePercent() {
+        return extendedChangePercent;
+    }
+
+    public void setExtendedChangePercent(String extendedChangePercent) {
+        this.extendedChangePercent = extendedChangePercent;
+    }
+
+    public String getExtendedPrice() {
+        return extendedPrice;
+    }
+
+    public void setExtendedPrice(String extendedPrice) {
+        this.extendedPrice = extendedPrice;
+    }
+
+    public String getExtendedPriceTime() {
+        return extendedPriceTime;
+    }
+
+    public void setExtendedPriceTime(String extendedPriceTime) {
+        this.extendedPriceTime = extendedPriceTime;
+    }
+
+    public String getHigh() {
+        return high;
+    }
+
+    public void setHigh(String high) {
+        this.high = high;
+    }
+
+    public String getIexAskPrice() {
+        return iexAskPrice;
+    }
+
+    public void setIexAskPrice(String iexAskPrice) {
+        this.iexAskPrice = iexAskPrice;
+    }
+
+    public String getIexAskSize() {
+        return iexAskSize;
+    }
+
+    public void setIexAskSize(String iexAskSize) {
+        this.iexAskSize = iexAskSize;
+    }
+
+    public String getIexBidPrice() {
+        return iexBidPrice;
+    }
+
+    public void setIexBidPrice(String iexBidPrice) {
+        this.iexBidPrice = iexBidPrice;
+    }
+
+    public String getIexBidSize() {
+        return iexBidSize;
+    }
+
+    public void setIexBidSize(String iexBidSize) {
+        this.iexBidSize = iexBidSize;
+    }
+
+    public String getIexLastUpdated() {
+        return iexLastUpdated;
+    }
+
+    public void setIexLastUpdated(String iexLastUpdated) {
+        this.iexLastUpdated = iexLastUpdated;
+    }
+
+    public String getIexMarketPercent() {
+        return iexMarketPercent;
+    }
+
+    public void setIexMarketPercent(String iexMarketPercent) {
+        this.iexMarketPercent = iexMarketPercent;
+    }
+
+    public String getIexRealtimePrice() {
+        return iexRealtimePrice;
+    }
+
+    public void setIexRealtimePrice(String iexRealtimePrice) {
+        this.iexRealtimePrice = iexRealtimePrice;
+    }
+
+    public String getIexRealtimeSize() {
+        return iexRealtimeSize;
+    }
+
+    public void setIexRealtimeSize(String iexRealtimeSize) {
+        this.iexRealtimeSize = iexRealtimeSize;
+    }
+
+    public String getIexVolume() {
+        return iexVolume;
+    }
+
+    public void setIexVolume(String iexVolume) {
+        this.iexVolume = iexVolume;
+    }
+
+    public String getLatestPrice() {
+        return latestPrice;
+    }
+
+    public void setLatestPrice(String latestPrice) {
+        this.latestPrice = latestPrice;
+    }
+
+    public String getLatestSource() {
+        return latestSource;
+    }
+
+    public void setLatestSource(String latestSource) {
+        this.latestSource = latestSource;
+    }
+
+    public String getLatestTime() {
+        return latestTime;
+    }
+
+    public void setLatestTime(String latestTime) {
+        this.latestTime = latestTime;
+    }
+
+    public String getLatestUpdate() {
+        return latestUpdate;
+    }
+
+    public void setLatestUpdate(String latestUpdate) {
+        this.latestUpdate = latestUpdate;
+    }
+
+    public String getLatestVolume() {
+        return latestVolume;
+    }
+
+    public void setLatestVolume(String latestVolume) {
+        this.latestVolume = latestVolume;
+    }
+
+    public String getLow() {
+        return low;
+    }
+
+    public void setLow(String low) {
+        this.low = low;
+    }
+
+    public String getMarketCap() {
+        return marketCap;
+    }
+
+    public void setMarketCap(String marketCap) {
+        this.marketCap = marketCap;
+    }
+
+    public String getOpen() {
+        return open;
+    }
+
+    public void setOpen(String open) {
+        this.open = open;
+    }
+
+    public String getOpenTime() {
+        return openTime;
+    }
+
+    public void setOpenTime(String openTime) {
+        this.openTime = openTime;
+    }
+
+    public String getPeRatio() {
+        return peRatio;
+    }
+
+    public void setPeRatio(String peRatio) {
+        this.peRatio = peRatio;
+    }
+
+    public String getPreviousClose() {
+        return previousClose;
+    }
+
+    public void setPreviousClose(String previousClose) {
+        this.previousClose = previousClose;
+    }
+
+    public String getPrimaryExchange() {
+        return primaryExchange;
+    }
+
+    public void setPrimaryExchange(String primaryExchange) {
+        this.primaryExchange = primaryExchange;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getWeek52High() {
+        return week52High;
+    }
+
+    public void setWeek52High(String week52High) {
+        this.week52High = week52High;
+    }
+
+    public String getWeek52Low() {
+        return week52Low;
+    }
+
+    public void setWeek52Low(String week52Low) {
+        this.week52Low = week52Low;
+    }
+
+    public String getYtdChange() {
+        return ytdChange;
+    }
+
+    public void setYtdChange(String ytdChange) {
+        this.ytdChange = ytdChange;
+    }
+
+    @Override
+    public String getId() {
+        return symbol;
+    }
+
+    @Override
+    public void setId(String symbol) {
+        this.symbol = symbol;
+    }
+}
