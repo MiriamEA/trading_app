a6ad1aba05aa79cc256b7deeda6c09196ad1b451
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-17 16:00:29 +0000

    AppConfig added

---
+++ src/main/java/ca/jrvs/apps/trading/AppConfig.java
@@ -0,0 +1,24 @@
+package ca.jrvs.apps.trading;
+
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class AppConfig {
+
+    private Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
+    @Bean
+    public HttpClientConnectionManager httpClientConnectionManager() {
+        PoolingHttpClientConnectionManager manager = new PoolingHttpClientConnectionManager();
+        manager.setMaxTotal(50);
+        manager.setDefaultMaxPerRoute(50);
+        return manager;
+    }
+}
