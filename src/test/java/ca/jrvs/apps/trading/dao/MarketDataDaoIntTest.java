fc4c8a310b895c89d5693ae29ebcb303387ced31
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-18 20:05:46 +0000

    MarketDataDao test added

--- src/test/java/ca/jrvs/apps/trading/dao/MarketDataDaoIntTest.java
+++ src/test/java/ca/jrvs/apps/trading/dao/MarketDataDaoIntTest.java
@@ -2,7 +2,6 @@ package ca.jrvs.apps.trading.dao;
 
 import ca.jrvs.apps.trading.model.config.MarketDataConfig;
 import ca.jrvs.apps.trading.model.domain.IexQuote;
-import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,33 +10,52 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class MarketDataDaoIntTest {
+
     private MarketDataDao marketDataDao;
 
     @Before
     public void setup() {
-        HttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager();
-        MarketDataConfig config = new MarketDataConfig();
-        marketDataDao = new MarketDataDao(connectionManager, config);
+        marketDataDao = new MarketDataDao(new BasicHttpClientConnectionManager(), new MarketDataConfig());
     }
 
     @Test
     public void findIexQuoteByTicker() {
-        String ticker = "AAPL";
-        IexQuote quote = marketDataDao.findIexQuoteByTicker(ticker);
-        assertEquals(ticker, quote.getSymbol());
+        String ticker = "MCD";
+        IexQuote iexQuote = marketDataDao.findIexQuoteByTicker(ticker);
+        assertNotNull(iexQuote);
+        assertEquals(ticker, iexQuote.getSymbol());
+
+        try {
+            IexQuote iexQuote1 = marketDataDao.findIexQuoteByTicker("MCD2");
+            fail();
+        } catch (ResourceNotFoundException e) {
+
+        }
     }
 
     @Test
-    public void findIexQuoteByTickerList() {
-        List<String> tickerList = new LinkedList<>();
-        tickerList.addAll(Arrays.asList("AAPL", "FB"));
-        List<IexQuote> iexQuotes = marketDataDao.findIexQuoteByTickers(tickerList);
-        assertEquals(tickerList.size(), iexQuotes.size());
-        assertTrue(tickerList.contains(iexQuotes.get(0).getSymbol()));
-        assertTrue(tickerList.contains(iexQuotes.get(1).getSymbol()));
+    public void findIexQuoteByTickers() {
+        List<String> tickers = Arrays.asList("MCD", "AAPL", "ACDVF");
+        List<IexQuote> iexQuotes = marketDataDao.findIexQuoteByTickers(tickers);
+        assertNotNull(iexQuotes);
+        assertEquals(3, iexQuotes.size());
+        List<String> quoteTickers = new LinkedList<>();
+        for (IexQuote q : iexQuotes) {
+            quoteTickers.add(q.getSymbol());
+        }
+        assertTrue(quoteTickers.contains("AAPL"));
+        assertTrue(quoteTickers.contains("MCD"));
+        assertTrue(quoteTickers.contains("ACDVF"));
+
+        List<String> tickers1 = Arrays.asList("AAPL", "MCD2");
+        try {
+            List<IexQuote> result = marketDataDao.findIexQuoteByTickers(tickers1);
+            fail();
+        } catch (IllegalArgumentException e) {
+
+        }
     }
 }
