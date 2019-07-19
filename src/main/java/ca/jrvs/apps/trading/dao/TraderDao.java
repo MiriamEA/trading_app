a95c4b13f3ca8a8ab64b5f767154f4b3a5a76394
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-19 18:48:13 +0000

    TraderDao added

---
+++ src/main/java/ca/jrvs/apps/trading/dao/TraderDao.java
@@ -0,0 +1,51 @@
+package ca.jrvs.apps.trading.dao;
+
+import ca.jrvs.apps.trading.model.domain.Trader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+
+public class TraderDao extends JdbcCrudDao<Trader, Integer> {
+    private final static String TABLE_NAME = "trader";
+    private final static String ID_NAME = "id";
+    private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert simpleJdbcInsert;
+
+    @Autowired
+    public TraderDao(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        simpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName(TABLE_NAME).usingGeneratedKeyColumns(ID_NAME);
+    }
+
+    @Override
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    @Override
+    public SimpleJdbcInsert getSimpleJdbcInsert() {
+        return simpleJdbcInsert;
+    }
+
+    @Override
+    public String getTableName() {
+        return TABLE_NAME;
+    }
+
+    @Override
+    public Class getIdClass() {
+        return Integer.class;
+    }
+
+    @Override
+    public String getIdName() {
+        return ID_NAME;
+    }
+
+    @Override
+    public Class getEntityClass() {
+        return Trader.class;
+    }
+}
