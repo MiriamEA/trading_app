fcfcb496f8d9f37aae7360512c52e4bfcf786a9b
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-22 17:06:21 +0000

    isEmpty fixed

--- src/main/java/ca/jrvs/apps/trading/util/StringUtil.java
+++ src/main/java/ca/jrvs/apps/trading/util/StringUtil.java
@@ -2,12 +2,12 @@ package ca.jrvs.apps.trading.util;
 
 public class StringUtil {
 
-    public static boolean isEmpty(String s){
-        return s != null && !s.equals("");
+    public static boolean isEmpty(String s1, String s2, String s3){
+        return isEmpty(s1) || isEmpty(s2) || isEmpty(s3);
     }
 
-    public static boolean isEmpty(String s1, String s2, String s3){
-        return isEmpty(s1) && isEmpty(s2) && isEmpty(s3);
+    public static boolean isEmpty(String s) {
+        return s == null || s.equals("");
     }
 
 }
