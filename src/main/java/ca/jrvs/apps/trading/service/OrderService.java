a9fdb67cc958055cd69849d65100a5424110add5
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-22 17:31:37 +0000

    OrderService added

---
+++ src/main/java/ca/jrvs/apps/trading/service/OrderService.java
@@ -0,0 +1,54 @@
+package ca.jrvs.apps.trading.service;
+
+import ca.jrvs.apps.trading.dao.AccountDao;
+import ca.jrvs.apps.trading.dao.PositionDao;
+import ca.jrvs.apps.trading.dao.QuoteDao;
+import ca.jrvs.apps.trading.dao.SecurityOrderDao;
+import ca.jrvs.apps.trading.model.domain.SecurityOrder;
+import ca.jrvs.apps.trading.model.dto.MarketOrderDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class OrderService {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
+
+    private AccountDao accountDao;
+    private SecurityOrderDao securityOrderDao;
+    private QuoteDao quoteDao;
+    private PositionDao positionDao;
+
+    @Autowired
+    public OrderService(AccountDao accountDao, SecurityOrderDao securityOrderDao, QuoteDao quoteDao, PositionDao positionDao) {
+        this.accountDao = accountDao;
+        this.securityOrderDao = securityOrderDao;
+        this.quoteDao = quoteDao;
+        this.positionDao = positionDao;
+    }
+
+    /**
+     * Execute a market order
+     * - validate the order (e.g. size, and ticker)
+     * - Create a securityOrder (for security_order table)
+     * - Handle buy or sell order
+     * - buy order : check account balance
+     * - sell order: check position for the ticker/symbol
+     * - (please don't forget to update securityOrder.status)
+     * - Save and return securityOrder
+     * NOTE: you will need to some helper methods (protected or private)
+     *
+     * @param orderDto market order
+     * @return SecurityOrder from security_order table
+     * @throws org.springframework.dao.DataAccessException if unable to get data from DAO
+     * @throws IllegalArgumentException                    for invalid input
+     */
+    public SecurityOrder executeMarketOrder(MarketOrderDto orderDto) {
+        SecurityOrder securityOrder = new SecurityOrder();
+        return securityOrder;
+    }
+}
