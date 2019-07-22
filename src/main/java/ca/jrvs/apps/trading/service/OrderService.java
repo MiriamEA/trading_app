f0026e97f392936fb697c78b421f12390e0a596d
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-22 19:25:33 +0000

    OrderService done

--- src/main/java/ca/jrvs/apps/trading/service/OrderService.java
+++ src/main/java/ca/jrvs/apps/trading/service/OrderService.java
@@ -4,13 +4,19 @@ import ca.jrvs.apps.trading.dao.AccountDao;
 import ca.jrvs.apps.trading.dao.PositionDao;
 import ca.jrvs.apps.trading.dao.QuoteDao;
 import ca.jrvs.apps.trading.dao.SecurityOrderDao;
+import ca.jrvs.apps.trading.model.domain.Account;
+import ca.jrvs.apps.trading.model.domain.OrderStatus;
+import ca.jrvs.apps.trading.model.domain.Position;
 import ca.jrvs.apps.trading.model.domain.SecurityOrder;
 import ca.jrvs.apps.trading.model.dto.MarketOrderDto;
+import ca.jrvs.apps.trading.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static java.lang.Math.abs;
 
 @Service
 @Transactional
@@ -48,7 +54,68 @@ public class OrderService {
      * @throws IllegalArgumentException                    for invalid input
      */
     public SecurityOrder executeMarketOrder(MarketOrderDto orderDto) {
+        validateOrder(orderDto);
+
+        String ticker = orderDto.getTicker();
+        int orderSize = orderDto.getSize();
+        int accountId = orderDto.getAccountId();
         SecurityOrder securityOrder = new SecurityOrder();
+        securityOrder.setAccountId(accountId);
+        securityOrder.setSize(orderSize);
+        securityOrder.setTicker(ticker);
+        double price = quoteDao.findById(ticker).getAskPrice();
+        securityOrder.setPrice(price);
+        Account account = accountDao.findById(accountId);
+        double totalPrice = price * abs(orderSize);
+
+        if (orderSize > 0) {
+            return executeBuying(totalPrice, securityOrder, account);
+        } else {
+            return executeSelling(totalPrice, securityOrder, account);
+        }
+    }
+
+    /**
+     * Checks that an order has a nonzero size and a nonempty ticker
+     *
+     * @param order must not be null
+     * @throws IllegalArgumentException if size is 0, or if ticker is empty
+     */
+    private void validateOrder(MarketOrderDto order) {
+        int orderSize = order.getSize();
+        if (orderSize == 0) {
+            throw new IllegalArgumentException("Order size cannot be 0.");
+        }
+        String ticker = order.getTicker();
+        if (StringUtil.isEmpty(ticker)) {
+            throw new IllegalArgumentException("Ticker cannot be empty.");
+        }
+    }
+
+    private SecurityOrder executeBuying(double totalPrice, SecurityOrder securityOrder, Account account) {
+        if (totalPrice > account.getAmount()) {
+            securityOrder.setStatus(OrderStatus.CANCELED);
+            securityOrderDao.save(securityOrder);
+            throw new IllegalArgumentException("Insufficient fund. Reqired: " + totalPrice + ", availabel: " + account.getAmount());
+        } else {
+            securityOrder.setStatus(OrderStatus.FILLED);
+            accountDao.updateAmountById(account.getAmount() - totalPrice, account.getId());
+            securityOrderDao.save(securityOrder);
+        }
+        return securityOrder;
+    }
+
+    private SecurityOrder executeSelling(double totalPrice, SecurityOrder securityOrder, Account account) {
+        Position position = positionDao.findByAccountIdAndTicker(account.getId(), securityOrder.getTicker());
+        if (position.getPosition() < abs(securityOrder.getSize())) {
+            securityOrder.setStatus(OrderStatus.CANCELED);
+            securityOrderDao.save(securityOrder);
+            throw new IllegalArgumentException("Not enough ");
+        } else {
+            securityOrder.setStatus(OrderStatus.FILLED);
+            accountDao.updateAmountById(account.getAmount() + totalPrice, account.getId());
+            securityOrderDao.save(securityOrder);
+        }
         return securityOrder;
     }
 }
