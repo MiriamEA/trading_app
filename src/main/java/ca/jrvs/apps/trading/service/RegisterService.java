fc6043c6966dd8d369503f7c9c744125311194e8
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-23 16:22:17 +0000

    email validation added

--- src/main/java/ca/jrvs/apps/trading/service/RegisterService.java
+++ src/main/java/ca/jrvs/apps/trading/service/RegisterService.java
@@ -1,6 +1,5 @@
 package ca.jrvs.apps.trading.service;
 
-
 import ca.jrvs.apps.trading.dao.AccountDao;
 import ca.jrvs.apps.trading.dao.PositionDao;
 import ca.jrvs.apps.trading.dao.SecurityOrderDao;
@@ -9,6 +8,7 @@ import ca.jrvs.apps.trading.model.domain.Account;
 import ca.jrvs.apps.trading.model.domain.Position;
 import ca.jrvs.apps.trading.model.domain.Trader;
 import ca.jrvs.apps.trading.model.view.TraderAccountView;
+import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,8 +24,7 @@ public class RegisterService {
     private SecurityOrderDao securityOrderDao;
 
     @Autowired
-    public RegisterService(TraderDao traderDao, AccountDao accountDao,
-                           PositionDao positionDao, SecurityOrderDao securityOrderDao) {
+    public RegisterService(TraderDao traderDao, AccountDao accountDao, PositionDao positionDao, SecurityOrderDao securityOrderDao) {
         this.traderDao = traderDao;
         this.accountDao = accountDao;
         this.positionDao = positionDao;
@@ -37,14 +36,12 @@ public class RegisterService {
      *
      * @param trader trader info
      * @return traderAccountView for the new trader
-     * @throws org.springframework.dao.DataAccessException        if unable to retrieve data
-     * @throws IllegalArgumentException                           for invalid input
+     * @throws org.springframework.dao.DataAccessException if unable to retrieve data
+     * @throws IllegalArgumentException                    for invalid input
      */
     public TraderAccountView createTraderAndAccount(Trader trader) {
-        boolean traderValid = validateTrader(trader);
-        if (!traderValid) {
-            throw new IllegalArgumentException("All attributes of a trader must be set.");
-        }
+        validateTrader(trader);
+
         trader = traderDao.save(trader);
         Account account = new Account(trader.getId());
         account = accountDao.save(account);
@@ -52,21 +49,29 @@ public class RegisterService {
     }
 
     /**
-     * Checks whether all fields of a trader at not null
+     * Checks whether all fields of a trader at not null and whether email is valid
      *
      * @param trader trader to check
-     * @return true if the trader is not null and all fields (except id) are not null, false otherwise
+     * @throws IllegalArgumentException if trader is null, a field of trader is null, email is invalid
      */
-    private boolean validateTrader(Trader trader) {
-        if(trader == null){
-            return false;
+    private void validateTrader(Trader trader) {
+        if (trader == null) {
+            throw new IllegalArgumentException("Trader cannot be null.");
         }
         boolean firstNameIsSet = trader.getFirstName() != null;
         boolean lastNameIsSet = trader.getLastName() != null;
         boolean countryIsSet = trader.getCountry() != null;
         boolean dobIsSet = trader.getDob() != null;
         boolean emailIsSet = trader.getEmail() != null;
-        return firstNameIsSet && lastNameIsSet && countryIsSet && dobIsSet && emailIsSet;
+        boolean traderNotEmpty = firstNameIsSet && lastNameIsSet && countryIsSet && dobIsSet && emailIsSet;
+        if (!traderNotEmpty) {
+            throw new IllegalArgumentException("All attributes of a trader must be set.");
+        }
+
+        boolean isValidEmail = EmailValidator.getInstance().isValid(trader.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalArgumentException("Email is invalid.");
+        }
     }
 
     /**
@@ -78,12 +83,12 @@ public class RegisterService {
      * @throws IllegalArgumentException                           for invalid input
      */
     public void deleteTraderById(Integer traderId) {
-        if(traderId == null){
+        if (traderId == null) {
             throw new IllegalArgumentException("TraderId cannot be null.");
         }
         Account account = accountDao.findByTraderId(traderId);
         int accountId = account.getId();
-        if(account.getAmount() != 0){
+        if (account.getAmount() != 0) {
             throw new IllegalArgumentException("Trader " + traderId + " still has money and cannot be deleted.");
         }
         List<Position> positions = positionDao.findAllByAccountId(accountId);
@@ -96,5 +101,4 @@ public class RegisterService {
         accountDao.deleteById(accountId);
         traderDao.deleteById(traderId);
     }
-
 }
