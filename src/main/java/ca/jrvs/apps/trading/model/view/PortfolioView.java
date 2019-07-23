ebbb6282c099a6ff5c1e19e11517b2b38854cf89
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-23 15:14:44 +0000

    PortfolioView updated

--- src/main/java/ca/jrvs/apps/trading/model/view/PortfolioView.java
+++ src/main/java/ca/jrvs/apps/trading/model/view/PortfolioView.java
@@ -3,11 +3,16 @@ package ca.jrvs.apps.trading.model.view;
 import ca.jrvs.apps.trading.model.domain.Position;
 import ca.jrvs.apps.trading.model.domain.Quote;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class PortfolioView {
 
     private List<SecurityRow> securityRows;
+
+    public PortfolioView() {
+        securityRows = new LinkedList<>();
+    }
 
     public List<SecurityRow> getSecurityRows() {
         return securityRows;
@@ -17,10 +22,24 @@ public class PortfolioView {
         this.securityRows = securityRows;
     }
 
+    public void addSecurityRow(Position position, Quote quote, String ticker) {
+        SecurityRow securityRow = new SecurityRow(position, quote, ticker);
+        securityRows.add(securityRow);
+    }
+
     class SecurityRow {
         private Position position;
         private Quote quote;
         private String ticker;
+
+        public SecurityRow() {
+        }
+
+        public SecurityRow(Position position, Quote quote, String ticker) {
+            this.position = position;
+            this.quote = quote;
+            this.ticker = ticker;
+        }
 
         public Position getPosition() {
             return position;
