16f6a69fa01df82685b5732d46aee7c25df6d4d3
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-23 15:40:37 +0000

    Json annotations added

--- src/main/java/ca/jrvs/apps/trading/model/view/PortfolioView.java
+++ src/main/java/ca/jrvs/apps/trading/model/view/PortfolioView.java
@@ -2,22 +2,29 @@ package ca.jrvs.apps.trading.model.view;
 
 import ca.jrvs.apps.trading.model.domain.Position;
 import ca.jrvs.apps.trading.model.domain.Quote;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import java.util.LinkedList;
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PortfolioView {
 
+    @JsonProperty("securityRows")
     private List<SecurityRow> securityRows;
 
     public PortfolioView() {
         securityRows = new LinkedList<>();
     }
 
+    @JsonProperty("securityRows")
     public List<SecurityRow> getSecurityRows() {
         return securityRows;
     }
 
+    @JsonProperty("securityRows")
     public void setSecurityRows(List<SecurityRow> securityRows) {
         this.securityRows = securityRows;
     }
@@ -27,9 +34,14 @@ public class PortfolioView {
         securityRows.add(securityRow);
     }
 
+    @JsonPropertyOrder({"ticker", "position", "quote"})
     class SecurityRow {
+
+        @JsonProperty("ticker")
         private Position position;
+        @JsonProperty("positon")
         private Quote quote;
+        @JsonProperty("quote")
         private String ticker;
 
         public SecurityRow() {
@@ -41,26 +53,32 @@ public class PortfolioView {
             this.ticker = ticker;
         }
 
+        @JsonProperty("positon")
         public Position getPosition() {
             return position;
         }
 
+        @JsonProperty("positon")
         public void setPosition(Position position) {
             this.position = position;
         }
 
+        @JsonProperty("quote")
         public Quote getQuote() {
             return quote;
         }
 
+        @JsonProperty("quote")
         public void setQuote(Quote quote) {
             this.quote = quote;
         }
 
+        @JsonProperty("ticker")
         public String getTicker() {
             return ticker;
         }
 
+        @JsonProperty("ticker")
         public void setTicker(String ticker) {
             this.ticker = ticker;
         }
--- src/main/java/ca/jrvs/apps/trading/model/view/TraderAccountView.java
+++ src/main/java/ca/jrvs/apps/trading/model/view/TraderAccountView.java
@@ -1,11 +1,18 @@
 package ca.jrvs.apps.trading.model.view;
 
-import ca.jrvs.apps.trading.model.domain.Trader;
 import ca.jrvs.apps.trading.model.domain.Account;
+import ca.jrvs.apps.trading.model.domain.Trader;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"trader", "account"})
 public class TraderAccountView {
 
+    @JsonProperty("account")
     private Account account;
+    @JsonProperty("trader")
     private Trader trader;
 
     public TraderAccountView() {
@@ -16,18 +23,22 @@ public class TraderAccountView {
         this.trader = trader;
     }
 
+    @JsonProperty("account")
     public Account getAccount() {
         return account;
     }
 
+    @JsonProperty("account")
     public void setAccount(Account account) {
         this.account = account;
     }
 
+    @JsonProperty("trader")
     public Trader getTrader() {
         return trader;
     }
 
+    @JsonProperty("trader")
     public void setTrader(Trader trader) {
         this.trader = trader;
     }
