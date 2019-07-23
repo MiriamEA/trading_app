91154600d4a24ef4636b12c11151d64dd7469f96
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-23 12:59:08 +0000

    OrderController done

---
+++ src/main/java/ca/jrvs/apps/trading/controller/OrderController.java
@@ -0,0 +1,37 @@
+package ca.jrvs.apps.trading.controller;
+
+import ca.jrvs.apps.trading.model.domain.SecurityOrder;
+import ca.jrvs.apps.trading.model.dto.MarketOrderDto;
+import ca.jrvs.apps.trading.service.OrderService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+    private OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @ApiOperation(value = "Submit a market order", notes = "Submit a market order")
+    @PostMapping("/marketOrder")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public SecurityOrder executeMarketOrder(MarketOrderDto order) {
+        try {
+            return orderService.executeMarketOrder(order);
+        } catch (Exception e) {
+            throw ResponseExceptionUtil.getResponseStatusException(e);
+        }
+    }
+}
