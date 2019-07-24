0d5e423bad829702a62af9a78c5088cfa8802812
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-24 15:12:44 +0000

    AccountDaoTest added

---
+++ src/test/java/ca/jrvs/apps/trading/dao/AccountDaoTest.java
@@ -0,0 +1,72 @@
+package ca.jrvs.apps.trading.dao;
+
+import ca.jrvs.apps.trading.model.domain.Account;
+import ca.jrvs.apps.trading.model.domain.Trader;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class AccountDaoTest {
+
+    private AccountDao accountDao;
+
+    @Before
+    public void setup() {
+        String jdbcUrl = "jdbc:postgresql://localhost/jrvstrading_test";
+        String user = "postgres";
+        String password = "password";
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+
+        TraderDao traderDao = new TraderDao(dataSource);
+        Trader trader = new Trader();
+        trader.setDob(new Date(2000, 1, 1));
+        trader.setLastName("Doe");
+        trader.setFirstName("John");
+        trader.setCountry("Canada");
+        trader.setEmail("johndoe@gmail.com");
+        traderDao.save(trader);
+
+        accountDao = new AccountDao(dataSource);
+        Account account = new Account();
+        account.setTraderId(1);
+        account.setId(1);
+        account.setAmount(100.0);
+        accountDao.save(account);
+    }
+
+    @Test
+    public void findByAccountId() {
+        Account account = accountDao.findByAccountId(1);
+        assertNotNull(account);
+        assertEquals(1, account.getId(), 0);
+        assertEquals(1, account.getTraderId(), 0);
+        assertEquals(100, account.getAmount(), 0);
+    }
+
+    @Test
+    public void findByTraderId() {
+        Account account = accountDao.findByTraderId(1);
+        assertNotNull(account);
+        assertEquals(1, account.getId(), 0);
+        assertEquals(1, account.getTraderId(), 0);
+        assertEquals(100, account.getAmount(), 0);
+    }
+
+    @Test
+    public void updateAmountById() {
+        accountDao.updateAmountById(1.5, 1);
+        Account account = accountDao.findByAccountId(1);
+        assertEquals(1, account.getId(), 0);
+        assertEquals(1.5, account.getAmount(), 0);
+    }
+}
