ddd97c95fb090c0bde26bf91a0d8332439908782
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-29 17:13:04 +0000

    App controller added

---
+++ src/main/java/ca/jrvs/apps/trading/controller/AppController.java
@@ -0,0 +1,18 @@
+package ca.jrvs.apps.trading.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class AppController {
+
+    @GetMapping("health")
+    @ResponseStatus(HttpStatus.OK)
+    public String health() {
+        return "I'm very healthy!";
+    }
+}
