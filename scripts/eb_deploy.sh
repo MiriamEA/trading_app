5aea6b0a67fd01d9961fdac57f6b985c56135af6
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-08-07 18:47:51 +0000

    eb_deploy.sh added

---
+++ scripts/eb_deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#exit if there is an error
+set -e
+
+if [-z "$1"]
+then
+    echo "usage $0 eb_env_name"
+    exit 1
+fi
+
+eb_env=$1
+
+rm -rf .elasticbeanstalk
+
+#init eb for the project
+eb init trading-app --platform java --region us-east-1
+eb use ${eb_env}
+
+#Edit eb config file
+cat >> .elasticbeanstalk/config.yml <<_EOF
+deploy:
+    artifact: target/trading-1.0-SNAPSHOT-elastic-beanstalk.zip
+_EOF
+
+#deploy
+eb deploy
+
+exit 0
